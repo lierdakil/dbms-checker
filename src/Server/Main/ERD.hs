e4209b1647a5194b1bfaa9c4fa5c4ab5fd3fc195
Nikolay Yakimov <root@livid.pp.ru>
2018-12-07 02:56:25 +0300

    Implement most of ERD interface

--- src/Server/Main/ERD.hs
+++ src/Server/Main/ERD.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Server.Main.ERD where
 
@@ -24,6 +27,13 @@ import Crypto.Hash (hash, Digest, SHA1)
 import Data.Char (isAlphaNum)
 import Control.Exception
 import System.IO.Error
+import API
+import API.Types
+import DB.Types
+import DB.Instances ()
+import DB.Accessor
+import DB.Utils
+import TutorialD.QQ
 
 
 erd :: ServerT (BasicCrud "erdId" ERDIdentifier) SessionEnv
@@ -37,16 +47,47 @@ erd = postErd
          :<|> putErd erdId
 
 getErd :: ERDIdentifier -> SessionEnv ERDBody
-getErd erdId = undefined
+getErd erdId = do
+  (uId, role) <- asks (liftM2 (,) userSessionUserId userSessionUserRole . sessionData)
+  let request Student = [tutdrel|ERDiagram where id = $erdId and userId = $uId|]
+      request Teacher = [tutdrel|ERDiagram where id = $erdId|]
+  (erds :: [ERDiagram]) <- fromRelation =<< execDB (request role)
+  when (null erds) $ throwError err404
+  return $ toResponseBody $ head erds
 
 postErd :: Text -> SessionEnv ERDBody
-postErd = undefined
+postErd desc = do
+  nid <- getNewId ERDIdentifier
+  uId <- asks (userSessionUserId . sessionData)
+  let erdiag = ERDiagram {
+          id = nid
+        , userId = uId
+        , diagram = desc
+        , accepted = NotAccepted
+        }
+  execDB [tutdctx|insert ERDiagram $erdiag|]
+  commitDB
+  return $ toResponseBody erdiag
 
 putErd :: ERDIdentifier -> Text -> SessionEnv ERDBody
-putErd = undefined
+putErd erdid desc = do
+  uId <- asks (userSessionUserId . sessionData)
+  execDB [tutdctx|update ERDiagram where id = $erdid and userId = $uId (
+    diagram := $desc, accepted := NotAccepted )|]
+  commitDB
+  return $ toResponseBody ERDiagram {
+      id = erdid
+    , userId = uId
+    , diagram = desc
+    , accepted = NotAccepted
+    }
 
 patchErd :: ERDIdentifier -> AcceptanceState -> SessionEnv ()
-patchErd = undefined
+patchErd erdid st = do
+  userRole <- asks (userSessionUserRole . sessionData)
+  when (userRole /= Teacher) $ throwError err403
+  execDB [tutdctx|update ERDiagram where id = $erdid ( accepted := $st )|]
+  commitDB
 
 renderErd :: ERDIdentifier -> SessionEnv FileData
 renderErd = undefined
