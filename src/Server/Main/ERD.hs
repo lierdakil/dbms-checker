c94736dc06b374ea9ceb078420a783a0fdb5e9be
Nikolay Yakimov <root@livid.pp.ru>
2018-12-07 02:57:11 +0300

    Clean imports

--- src/Server/Main/ERD.hs
+++ src/Server/Main/ERD.hs
@@ -8,28 +8,13 @@
 
 module Server.Main.ERD where
 
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as BL
 import Data.Text (Text)
-import qualified Data.Text as T
-import qualified Data.Text.Lazy as LT
-import qualified Data.Text.Lazy.Encoding as LT
-import qualified Data.Set as S
 import Control.Monad.Reader
 import Config
 import API
 import API.Types
 import DB.Types
 import Servant
-import Control.Monad.Error.Class
-import Data.Monoid ((<>))
-import Crypto.Hash (hash, Digest, SHA1)
-import Data.Char (isAlphaNum)
-import Control.Exception
-import System.IO.Error
-import API
-import API.Types
-import DB.Types
 import DB.Instances ()
 import DB.Accessor
 import DB.Utils
