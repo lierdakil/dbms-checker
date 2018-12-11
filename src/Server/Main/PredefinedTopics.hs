ccb174ae05347dae8b6e3937afa610cd117d6c06
Nikolay Yakimov <root@livid.pp.ru>
2018-12-11 18:07:45 +0300

    Only show free topics to students

--- src/Server/Main/PredefinedTopics.hs
+++ src/Server/Main/PredefinedTopics.hs
@@ -8,9 +8,22 @@ module Server.Main.PredefinedTopics where
 
 import Config
 import DB.Types
+import API.Types
 import DB.Instances ()
 import DB.Accessor
 import TutorialD.QQ
+import Control.Monad
 
 predefinedTopics :: SessionEnv [PredefinedTopic]
-predefinedTopics = bracketDB $ fromRelation =<< execDBRel [tutdrel|PredefinedTopic|]
+predefinedTopics = do
+  (uId, userRole) <- asks (
+    liftM2 (,) userInfoUserId userInfoUserRole . userSessionUserInfo . sessionData)
+  case userRole of
+    Teacher -> bracketDB $ fromRelation =<< execDBRel [tutdrel|PredefinedTopic|]
+    Student ->  bracketDB $ fromRelation =<< execDBRel
+      [tutdrel|(PredefinedTopic:{topic := PredefinedAssignedTopic @id}) not matching (
+        ((TopicAssignment join (
+          (User rename {id as userId}) matching (User where id = $uId){group}
+          )) where not userId = $uId
+          ){topic}
+        )|]
